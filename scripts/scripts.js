d5096c386ae993d0f1092f08be466917eb1f0fa3
RavinaRani <ravi0036@algonquinlive.com>
2025-05-15 11:03:27 -0400

    variable into in javascript

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+//console.log will out put a message in the developer console
+//two slashes start the single line comment
+console.log("it worked");//single line comment can go beside code
+
+/*
+this is a multi-line comment
+start with a/ and a* close with a * and a/
+*/
+
+//to declare a variable , use let or const
+const ourClassNum = "j207";
+let numStudents = 9; //with a number, you dont need quotes
+
+console.log(ourClassNum);
+console.log("numStudents");
+
+//if you declare a variable with let, you can change its value
+numStudent = 21;
+
+//if you declare a value with const, you can't change its value
+//ourClassNum ="C128";
+//(tring to change a const variable will give you an error, and syop the code)
+
+console.log(ourClassNum);
+
+//const and let can not be redeclared (or recreted)
+//let numStudents =25; (this will also give an error)
